f0827eaa84527d8b803b7bae953c294cb1680822
Uri Frischman <uri.frischman@gmail.com>
2017-07-07 12:01:03 -0400

    listview is setup with its adapter

--- app/src/main/java/com/frischman/uri/instagramclone/Profile/AccountSettingsActivity.java
+++ app/src/main/java/com/frischman/uri/instagramclone/Profile/AccountSettingsActivity.java
@@ -6,9 +6,13 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
 
 import com.frischman.uri.instagramclone.R;
+
+import java.util.ArrayList;
 
 /**
  * Created by Uri on 2017-07-07.
@@ -25,6 +29,7 @@ public class AccountSettingsActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accountsettings);
+        setupSettingsList();
 
         mBackButton = (ImageView) findViewById(R.id.backArrow);
         mBackButton.setOnClickListener(new View.OnClickListener() {
@@ -34,5 +39,20 @@ public class AccountSettingsActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+
+    private void setupSettingsList() {
+        ListView listView = (ListView) findViewById(R.id.lvAccountSettings);
+
+        ArrayList<String> options = new ArrayList<>();
+        options.add(getString(R.string.edit_your_profile));
+        options.add(getString(R.string.logout));
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(mContext, android.R.layout.simple_list_item_1, options);
+        listView.setAdapter(arrayAdapter);
+
+
+
     }
 }
