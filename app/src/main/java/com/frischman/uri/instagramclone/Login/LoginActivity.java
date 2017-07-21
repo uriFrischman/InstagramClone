3e031d1036c37d14514193890e40654e4f10f44d
Uri Frischman <uri.frischman@gmail.com>
2017-07-21 11:04:18 -0400

    created login and register activities

---
+++ app/src/main/java/com/frischman/uri/instagramclone/Login/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.frischman.uri.instagramclone.Login;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.frischman.uri.instagramclone.R;
+
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
